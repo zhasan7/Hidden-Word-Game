8698cf0be93b20e376c383fb96f34da1580e6db2
zhasan7 <zhasan@student-bmhs-la.org>
2023-01-04 13:41:34 -0800

    first pratice commit

---
+++ Game.java
@@ -0,0 +1,15 @@
+public class Game {
+    private String hidden;
+    private String guess;
+    private String hint;
+
+
+    public static void main(String[] args){
+        Game game = new Game();
+        game.run();
+    }
+
+    public void run() {
+        System.out.println("Zaki is the best");
+    }
+}
